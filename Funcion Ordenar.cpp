b257537f2468bc82cb2557deca4b6b3146c44454
Assalahu <dla.cruz2002@gmail.com>
2021-01-12 19:15:11 +0100

    ya está casi :)

--- Funcion Ordenar.cpp
+++ Funcion Ordenar.cpp
@@ -5,20 +5,21 @@
 
 using namespace std;
 
-typedef int tarray[13];
+const int LONGITUD_DEL_ARRAY = 1000;
+const int NUMERO_MAXIMO = 1000;
+
+typedef int tarray[LONGITUD_DEL_ARRAY];
 
 void ordenar(tarray &arr1, int longitud);
 
-int main()
-{
-    srand(time(NULL));
-    int a, b, c, d, e, f, g, h, i, j, k, l, m;
-    
+int main() {
+    srand(time(NULL));    
     tarray arr1;
-    for (int i = 0; i < 13; i++) {
-        arr1[i] = rand() % 100;
-    }
+
     int longitud = sizeof(arr1) / sizeof(*arr1);
+    for (int i = 0; i < longitud; i++) {
+        arr1[i] = rand() % NUMERO_MAXIMO;
+    }
     for (int d = 0; d < longitud; d++) {
         cout << arr1[d] << ", ";
 
@@ -33,12 +34,32 @@ void ordenar(tarray& arr1, int longitud) {
     for (int a = 0; a < longitud; a++) {  //Copiamos el array
         arr2[a] = arr1[a];
     }
-    for (int d = 0; d < longitud; d++) {
-        cout << arr1[d] << ", ";
-    }
 
     for (int i = 0; i > -1; i++) { 
         i %= longitud;
+        //1 - Comprobamos si ya está odenado
+        contador1 = 0;
+        for (int b = 0; b < longitud - 1; b ++) { 
+            if (arr1[b] <= arr1[b + 1]) {
+                contador1++;
+            }
+        }
+        if (contador1 == longitud - 1) {
+            cout << endl;
+            for (int d = 0; d < longitud; d++) {
+                cout << arr1[d] << ", ";
+            }
+            break;
+        }
+        //2 - Imprimimos el array
+        // opcional
+        cout << endl;
+        for (int d = 0; d < longitud; d++) {
+            cout << arr1[d] << ", ";
+        }
+        cout << endl;
+
+        //3 - Ordenamos un numero
         for (int j = 0; j < longitud; j++) {
             if (arr1[i] < arr2[j]) {
                 arr1[i] = arr2[j];
@@ -48,23 +69,7 @@ void ordenar(tarray& arr1, int longitud) {
                 }
             }
         }
-        contador1 = 0;
-        for (int b = 0; b < longitud - 1; b ++) {
-            if (arr1[b] <= arr1[b + 1]) {
-                cout << endl << "a" << endl;
-                contador1++;
-            }
-        }
-        if (contador1 == longitud - 1) {
-            for (int d = 0; d < longitud; d++) {
-                cout << arr1[d] << ", ";
-            }
-            break;
-        }
-        cout << endl;
-        for (int d = 0; d < longitud; d++) {
-            cout << arr1[d] << ", ";
-        }
-        cout << endl;
     }
 }
+
+//Si se cambia el orden a 3, 2, 1
